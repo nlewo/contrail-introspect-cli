7075f008676c78006c6fb12906f99d232b635d42
lewo <lewo@abesis.fr>
2016-04-20 14:05:42 +0200

    Refactoring

--- src/github.com/nlewo/contrail-introspect-cli/contrail-introspect-cli.go
+++ src/github.com/nlewo/contrail-introspect-cli/contrail-introspect-cli.go
@@ -28,17 +28,6 @@ func load(url string, fromFile bool) *xml.XmlDocument{
 	return(doc)
 }
 
-func itf(vrouter string) {
-	url := "http://" + vrouter + ":8085/Snh_ItfReq"
-	var doc = load(url, false)
-	xps := xpath.Compile("//name/text()")
-	ss, _ := doc.Root().Search(xps)
-
-	for _, s := range ss {
-		fmt.Printf("%s\n", s)
-	}
-}
-
 func multiple(vrouter string, vrf_name string) {
 	url := "http://" + vrouter + ":8085" + "/Snh_PageReq?x=begin:-1,end:-1,table:" + vrf_name + ".uc.route.0,"
 	
@@ -51,49 +40,108 @@ func multiple(vrouter string, vrf_name string) {
 	}
 }
 
-func vrf(vrouter string) {
-	var url = "http://" + vrouter + ":8085" + "/Snh_VrfListReq"
-	var doc = load(url, false)
-	defer doc.Free()
-	xps := xpath.Compile("//vrf_list/list//name/text()")
-	ss, _ := doc.Root().Search(xps)
-	for _, s := range ss {
-		fmt.Printf("%s\n", s)
-	}
+type Page struct {
+	VrouterUrl string;
+	Table string;
 }
 
-func routeFromFile(filePath string) Collection {
-	var doc = load(filePath, true)
-	return route(doc)
+type File struct {
+	Path string;
 }
 
-func routeFromUrl(vrouter string, vrfName string) Collection {
-	var url = "http://" + vrouter + ":8085" + "/Snh_PageReq?x=begin:-1,end:-1,table:" + vrfName + ".uc.route.0,"
-	var doc = load(url, false)
-	return route(doc)
+type LoadAble interface {
+	Load(descCol DescCol) Collection;
 }
 
-
-func route(doc *xml.XmlDocument) Collection {
-	ss, err := doc.Root().Search("/__Inet4UcRouteResp_list/Inet4UcRouteResp/route_list/list")
-	if err != nil {
-		log.Fatal(err)
-	}
-	col := Collection{node: ss[0]}
+// Parse data to XML
+func fromDataToCollection(data []byte, descCol DescCol) Collection {
+	doc, _ := gokogiri.ParseXml(data)
+	ss, _ := doc.Search(descCol.BaseXpath)
+	col := Collection{node: ss[0], descCol: descCol}
 	return col
 }
 
-
-type Collection struct {
-	doc *xml.XmlDocument
-	node xml.Node
+func (file File) Load(descCol DescCol) Collection {
+	f, _ := os.Open(file.Path)
+	data, _ := ioutil.ReadAll(f)
+	return fromDataToCollection(data, descCol)
 }
 
-func routeList(col Collection) {
-	ss, _ := col.node.Search("RouteUcSandeshData/src_ip/text()")	
+func (page Page) Load(descCol DescCol) Collection {
+	url := "http://" + page.VrouterUrl + ":8085/Snh_PageReq?x=begin:-1,end:-1,table:" + page.Table + ","
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data, _ := ioutil.ReadAll(resp.Body)
+	return fromDataToCollection(data, descCol)
+}
+
+type Collection struct {
+	descCol DescCol;
+	doc *xml.XmlDocument;
+	node xml.Node;
+}
+
+type DescCol struct {
+	BaseXpath string;
+	ShortDetailXpath string;
+	LongDetailXpath []string;
+	DescElt DescElement;
+}
+
+type DescElement struct {
+	Short string;
+	Long []string;
+}
+
+type Element struct {
+	node xml.Node;
+	desc DescElement;
+}
+
+func BuildCol(p Page, d DescCol, e DescElement) Collection {
+	col := p.Load(d)
+	return col
+}
+
+func (col Collection) List() {
+	ss, _ := col.node.Search(col.descCol.ShortDetailXpath)	
 	for _, s := range ss {
 		fmt.Printf("%s\n", s)
 	}
+}
+
+func BuildItfPage(vrouter string) Page {
+	return Page{Table: "db.interface.0", VrouterUrl: vrouter}
+}
+func BuildVrfPage(vrouter string) Page {
+	return Page{Table: "db.vrf.0", VrouterUrl: vrouter}
+}
+func BuildRoutePage(vrouter string, vrfName string) Page {
+	return Page{Table: vrfName + ".uc.route.0,", VrouterUrl: vrouter}
+}
+func BuildRouteFile(path string) File {
+	return File{Path: path}
+}
+
+func DescItf() DescCol {
+	return DescCol{
+		BaseXpath: "__ItfResp_list/ItfResp/itf_list/list",
+		ShortDetailXpath: "ItfSandeshData/name/text()",
+	}
+}
+
+func DescRoute() DescCol {
+	return DescCol{
+		BaseXpath: "__Inet4UcRouteResp_list/Inet4UcRouteResp/route_list/list",
+		ShortDetailXpath: "RouteUcSandeshData/src_ip/text()"}
+}
+
+func DescVrf() DescCol {
+	return DescCol{
+		BaseXpath: "//vrf_list/list",
+		ShortDetailXpath: "//name/text()"}
 }
 
 func routeGet(c Collection, srcIp string) xml.Node {
@@ -146,7 +194,7 @@ func main() {
 					log.Fatal("Wrong argument number!")
 				}
 				vrouter := c.Args()[0]
-				itf(vrouter)
+				BuildItfPage(vrouter).Load(DescItf()).List()
 			},
 		},
 		{
@@ -168,22 +216,21 @@ func main() {
 				if c.NArg() != 1 {
 					log.Fatal("Wrong argument number!")
 				}
-				vrouter := c.Args()[0]
-				vrf(vrouter)
+				BuildVrfPage(c.Args()[0]).Load(DescVrf()).List()
 			},
 		},
 		{
 			Name:      "route",
 			Usage:     "route <vrouterUrl> <vrfName> [<srcIp>]",
 			Action: func(c *cli.Context) {
-				col := routeFromUrl(c.Args()[0], c.Args()[1])
+				col := BuildRoutePage(c.Args()[0], c.Args()[1]).Load(DescRoute())
 				switch c.NArg() {
 				case 2:
 					if showAsXml {
 						fmt.Printf("%s\n", col.node)
 						return
 					}
-					routeList(col)
+					col.List()
 				case 3:
 					route := routeGet(col, c.Args()[2])
 					if showAsXml {
@@ -197,14 +244,14 @@ func main() {
 		{
 			Name:      "route-from-file",
 			Action: func(c *cli.Context) {
-				col := routeFromFile(c.Args()[0])
+				col := BuildRouteFile(c.Args()[0]).Load(DescRoute())
 				switch c.NArg() {
 				case 1:
 					if showAsXml {
 						fmt.Printf("%s\n", col.node)
 						return
 					}
-					routeList(col)
+					col.List()
 				case 2:
 					route := routeGet(col, c.Args()[1])
 					if showAsXml {
